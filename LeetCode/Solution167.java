a12213c67d8b46f2c5f0109d8e2bdc93553f387b
vert <1379733223@qq.com>
2019-09-11 14:09:30 +0800

    Create Solution167.java

---
+++ LeetCode/Solution167.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+/**
+ * @author vert  @date 2019/9/11 13:23
+ */
+/*
+167. 两数之和 II - 输入有序数组(https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted/)
+给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+
+说明:
+返回的下标值（index1 和 index2）不是从零开始的。
+你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
+
+示例:
+输入: numbers = [2, 7, 11, 15], target = 9
+输出: [1,2]
+解释: 2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。
+ */
+public class Solution167 {
+    public static void main(String[] args) {
+        int[] numbers = {2, 7, 11, 15};
+        int target = 9;
+        Solution167 s = new Solution167();
+        System.out.println(Arrays.toString(s.twoSum(numbers,target)));
+    }
+    public int[] twoSum(int[] numbers, int target) {
+        int low = 0,high = numbers.length - 1;
+        while (low < high){
+            int sum = numbers[low] + numbers[high];
+            if (sum == target){
+                return new int[] {low + 1,high + 1};
+            }
+            else if (sum < target)  //总和小了，将其变大
+                low++;
+            else                    //总和大了，将其减小
+                high--;
+        }
+        return new int[] {-1,-1};
+    }
+}
