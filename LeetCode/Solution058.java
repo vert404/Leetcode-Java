1d9002c3d28c44172f56b39ce59874972de4eb40
vert <1379733223@qq.com>
2019-10-10 20:35:40 +0800

    Update Solution058.java

--- LeetCode/Solution058.java
+++ LeetCode/Solution058.java
@@ -1,8 +1,26 @@
 /**
  * @author vert  @date 2019/10/8 23:02
  */
+/*
+58. 最后一个单词的长度(https://leetcode-cn.com/problems/length-of-last-word/)
+给定一个仅包含大小写字母和空格 ' ' 的字符串，返回其最后一个单词的长度。
+如果不存在最后一个单词，请返回 0 。
+
+说明：一个单词是指由字母组成，但不包含任何空格的字符串。
+示例:
+输入: "Hello World"
+输出: 5
+ */
 public class Solution058 {
     public int lengthOfLastWord(String s) {
-        return 0;
+        int count = 0;
+        for (int i=s.length()-1;i>=0;i--){
+            if (s.charAt(i) != ' '){
+                count++;
+            }else if (count > 0){
+                return count;
+            }
+        }
+        return count;
     }
 }
