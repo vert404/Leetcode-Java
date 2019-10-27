efb6c4b2ffb3b15ec88c4dde6c1a498004ff9e65
vert <1379733223@qq.com>
2019-10-27 20:06:36 +0800

    Create Solution989.java

---
+++ LeetCode/Solution989.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+/**
+ * @author vert on 2019/10/27
+ */
+public class Solution989 {
+    public List<Integer> addToArrayForm(int[] A, int K) {
+        return null;
+    }
+}
